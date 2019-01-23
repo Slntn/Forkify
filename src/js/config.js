d817abccde9cf8fafbdaafb3d044974651810e9d
Slntn <slntn@yahoo.com>
2019-01-23 12:38:09 -0500

    test

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = 'd76ca6078b2ef8bba3c18d22b2c85c22';
---
+++ src/js/index.js
@@ -0,0 +1,103 @@
+import Search from './models/Search';
+import Recipe from './models/Recipe';
+import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
+import {elements, renderLoader, clearLoader} from './views/base';
+
+//Global state of the app
+// - Search object
+// - Current receipt object
+// - Shopping list object
+// - Linked receipt
+const state = {};
+
+/**
+ * Search Controller
+ */
+const controlSearch = async () => {
+    // get query
+    const query = searchView.getInput();
+
+    if (query){
+        //new Search object
+        state.search = new Search(query);
+
+        //TODO: Prepare UI for results
+        searchView.cleanInput();
+        searchView.clearResults();
+        renderLoader(elements.searchRes);
+
+        try{
+            await state.search.getResults();
+
+            //Render results
+            clearLoader();
+            searchView.renderResults(state.search.result);
+        } catch(err){
+            console.log(err);
+            alert("Something wrong with the search....");
+            clearLoader();
+        }
+
+        //Search for receipt
+        
+    }
+
+}
+
+elements.searchForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+    }
+});
+
+/**
+ * Recipe Controller
+ */
+const controlRecipe = async () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '');
+
+    if (id) {
+        // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+
+        // Highlight selected search item
+        if (state.search) searchView.highlightSelected(id);
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+
+        try {
+            // Get recipe data and parse ingredients
+            await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
+
+            // Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+    
+            // Render recipe
+            clearLoader();
+            recipeView.renderRecipe(
+                state.recipe,
+                state.likes.isLiked(id)
+            );
+
+        } catch (err) {
+            console.log(err);
+            alert('Error processing recipe!');
+        }
+    }
+};
+ 
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
